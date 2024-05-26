5efa774abb751391099b78211dcddf8b1b6d51c9
Eva Mae Miller <150403453+evmaemiller@users.noreply.github.com>
2024-05-26 21:05:27 +0000

    For Unit Test #4, changed written lines to display different lyrics

--- main.cs
+++ main.cs
@@ -2,9 +2,10 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Coming out of my cage");
-    Console.WriteLine ("And I’ve been doing just fine");
-    Console.WriteLine ("Gotta gotta be down");
-    Console.WriteLine ("Because I want it all");
+    Console.WriteLine ("Now, this is a story all about how");
+    Console.WriteLine ("My life got flipped-turned upside down");
+    Console.WriteLine ("And I’d like to take a minute");
+    Console.WriteLine ("Just sit right there");
+    Console.WriteLine ("I'll tell you how I became the prince of a town called Bel-Air");
   }
 }
