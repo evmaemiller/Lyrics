a6b40510e7ac7cdb4a94ddd98f4b1c3b24bc0806
Eva Mae Miller <150403453+evmaemiller@users.noreply.github.com>
2024-05-26 20:55:51 +0000

    For Unit Test #2, changed written lines to display different text

--- main.cs
+++ main.cs
@@ -2,9 +2,9 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Oh, well imagine /");
-    Console.WriteLine ("As I’m pacing the pew in a church corridor /");
-    Console.WriteLine ("And I can’t help but hear /");
-    Console.WriteLine ("No, I can’t help but hear an exchanging of words");
+    Console.WriteLine ("Oh-oh-oh-oh-oh,");
+    Console.WriteLine ("oh-oh-oh-oh,");
+    Console.WriteLine ("oh-oh-oh /");
+    Console.WriteLine ("Caught in a bad romance. . .");
   }
 }
