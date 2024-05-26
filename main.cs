81d425b671ae8ea80387954f6273b339efdbc3e0
Eva Mae Miller <150403453+evmaemiller@users.noreply.github.com>
2024-05-26 20:52:09 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Oh, well imagine /");
+    Console.WriteLine ("As I’m pacing the pew in a church corridor /");
+    Console.WriteLine ("And I can’t help but hear /");
+    Console.WriteLine ("No, I can’t help but hear an exchanging of words");
+  }
+}
